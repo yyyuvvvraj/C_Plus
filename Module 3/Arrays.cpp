23eb9011384e22bb9472bda993939c0e99399b9a
Yuvraj <yuvraj280605@gmail.com>
2025-07-09 20:12:05 +0530

    Initial commit

--- Module 3/Arrays.cpp
+++ Module 3/Arrays.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+    int i;
+    float scores[5];
+    cout << "Please input your array: " << endl;
+    for(i=0;i<5;i++){
+        cin >> scores[i];
+    }
+    cout << "The array you input is as follows: " << endl;
+    for(i=0;i<5;i++){
+        cout<< scores[i] << "\t";
+    }
+    return 0;
+}
